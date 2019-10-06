bf58fe6eba8689680d259e5cb207a51903d5b4f5
Cendekia Pramana Putra <me@cendekiapp.com>
2019-10-06 22:36:52 +0700

    add breadcrumb component

---
+++ src/components/Breadcrumb.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let segment;
+</script>
+
+<!-- Breadcrumb area -->
+<section class="cr-section breadcrumb-area" data-black-overlay="7">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<div class="cr-breadcrumb text-center">
+					<h2 class="cr-breadcrumb__title">{segment}</h2>
+					<div class="cr-breadcrumb__tree text-left text-md-left text-center">
+						<ul>
+							<li><a href=".">Beranda</a></li>
+							<li>{segment}</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section><!-- //Breadcrumb area -->
