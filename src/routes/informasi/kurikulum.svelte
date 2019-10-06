e85f83b7a095973b17a636b5d6e8da5f3a2f5ae4
Cendekia Pramana Putra <me@cendekiapp.com>
2019-10-06 23:07:17 +0700

    add 'kurikulum' page

--- src/routes/informasi/kurikulum.svelte
+++ src/routes/informasi/kurikulum.svelte
@@ -1,7 +1,32 @@
-<svelte:head>
-	<title>Kurikulum - Sekolah Quran Al-Ihsan</title>
-</svelte:head>
-
-<h1>Kurikulum - Sekolah Quran Al-Ihsan</h1>
-
-<p>This is the 'about' page. There's not much here.</p>
+<section class="cr-section about-area ptb--150 bg--white flower--left-top flower--right-bottom">
+    <div class="container">
+        <div class="about-details">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="about-details__content">
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12">
+                                <div class="single-block">
+                                    <h4 class="single-block__title">Kurikulum Kuttab:</h4>
+									<ol>
+										<li>Kurikulum Iman:</li>
+										<ol>
+											<li>Akidah</li>
+											<li>Akhlaq</li>
+											<li>Adab</li>
+										</ol>
+										<li>Kurikulum Quran: 1 tahun 1 Juz</li>
+									</ol>
+                                </div>
+								<div class="single-block">
+                                    <h4 class="single-block__title">Kurikulum Umum:</h4>
+									<p>Diajarkan materi yang diujikan dalam USBN (Matematika, Sain, Bahasa Indonesia), Bahasa Inggris dan Pengetahuan Umum.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
