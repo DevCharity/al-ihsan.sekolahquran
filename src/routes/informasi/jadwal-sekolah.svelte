f5e7ee0663f9423ba5134168824b19261732cb1d
Cendekia Pramana Putra <me@cendekiapp.com>
2019-10-06 23:33:58 +0700

    add 'jadwal sekolah' page

--- src/routes/informasi/jadwal-sekolah.svelte
+++ src/routes/informasi/jadwal-sekolah.svelte
@@ -1,7 +1,57 @@
-<svelte:head>
-	<title>Jadwal Sekolah - Sekolah Quran Al-Ihsan</title>
-</svelte:head>
-
-<h1>Jadwal Sekolah - Sekolah Quran Al-Ihsan</h1>
-
-<p>This is the 'about' page. There's not much here.</p>
+<section class="cr-section about-area ptb--150 bg--white flower--left-top flower--right-bottom">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-lg-12">
+				<div class="about-content">
+					<h3><strong>Jadwal kegiatan harian Sekolah Al-Quran Al-Ihsan</strong></h3>
+					<table class="table table-striped table-hover">
+						<thead>
+							<tr><th width="10%" scope="col">Waktu</th><th width="20%" scope="col">Kegiatan Harian</th><th scope="col">Tempat/Keterangan</th></tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>07.15 - 07.30</td>
+								<td>Sholat Dhuha</td>
+								<td>Di Masjid Al-Ihsan</td></tr>
+							<tr>
+								<td>07.30 - 07.45</td>
+								<td>Ikrar dan Do'a</td>
+								<td>Santri akan mendampatkan beberapa materi ikrar, seperti : Kisah-kisah dalam Al-Quran, Mufrodat bahasa arab, doa harian, dan hadist. Metode yang digunakan : mengikuti,mendengarkan, atau mengulang-ulang bersama .</td>
+							</tr>
+							<tr>
+								<td>07.45 - 09.30</td>
+								<td>Belajar Al-Quran</td>
+								<td>Santri belajar Al-Quran dengan metode talaqqi. Santri yang menunggu giliran melakukan kegiatan-kegiatan : muroja'ah bersama, memperbaiki bacaan, menulis khot, dsb. kegiatan ini dipandu oleh guru kelas.</td>
+							</tr>
+							<tr>
+								<td>09.30 - 10.00</td>
+								<td>Snack Time</td>
+								<td>Makan snack dikelas masing-masing (membawa makanan sehat)</td>
+							</tr>
+							<tr>
+								<td>10.00 - 11.30</td>
+								<td>KBM</td>
+								<td>Materi yang diajarkan berupa pengetahuan umum, sains, olahraga, bahasa dan matematika. Jadwal olahraga dilaksanakan diluar ruangan.</td>
+							</tr>
+							<tr>
+								<td>11.30 - 12.15</td>
+								<td>Sholat Dzuhur Berjamaah</td>
+								<td>Masjid Al-Ihsan</td>
+							</tr>
+							<tr>
+								<td>12.15 - 12.45</td>
+								<td>Makan Siang</td>
+								<td>Makan siang dikelas masing-masing (membawa makanan sehat)</td>
+							</tr>
+							<tr>
+								<td>12.45 - 13.00</td>
+								<td>Evaluasi dan Pulang</td>
+								<td>Santri melakukan evaluasi pembelajaran , kemudia pulang.</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
