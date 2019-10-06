8a2361d5a7962a3ef1ada49dba3dce5631ec72c7
Cendekia Pramana Putra <me@cendekiapp.com>
2019-10-06 22:37:12 +0700

    refactor the page

---
+++ src/routes/informasi/_layout.svelte
@@ -0,0 +1,42 @@
+<script>
+    export let segment;
+    
+    function titleCase(str) {
+        var removeDash = str.replace(/-/g, ' ');
+        var splitStr = removeDash.toLowerCase().split(' ');
+        for (var i = 0; i < splitStr.length; i++) {
+            splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+        }
+
+        return splitStr.join(' '); 
+    }
+</script>
+
+<svelte:head>
+	<title>{titleCase(segment)} - Sekolah Quran Al-Ihsan</title>
+</svelte:head>
+
+<!-- Breadcrumb area -->
+<section class="cr-section breadcrumb-area" data-black-overlay="7">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<div class="cr-breadcrumb text-center">
+					<h2 class="cr-breadcrumb__title">{titleCase(segment)}</h2>
+					<div class="cr-breadcrumb__tree text-left text-md-left text-center">
+						<ul>
+							<li><a href=".">Beranda</a></li>
+							<li>{titleCase(segment)}</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section><!-- //Breadcrumb area -->
+
+
+<!-- Page Conent -->
+<main class="page-content">
+	<slot></slot>
+</main>
